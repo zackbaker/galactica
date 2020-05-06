d1c8ae7108b9d1966acc097fda49dfc5d12128d1
Zack Baker <zbakerdesign@gmail.com>
2020-05-06 16:10:29 -0600

    Added enemy laser and have enemies shoot one laser while the player shoots another

--- ships/player.py
+++ ships/player.py
@@ -9,6 +9,7 @@ class Player(Base):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.velocity = 5
+        self.laser_img = 'player_laser.png'
         # TODO: Figure this mess out. See if there is a better way
         dirname = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         self.img = pygame.image.load(os.path.join(dirname, 'assets', 'player_ship.png'))
