e7454b0ea289790beb1bd36edfecd17c8b9ec4f2
Zack Baker <zbakerdesign@gmail.com>
2020-05-06 14:57:30 -0600

    Added enemy spawns based on current level

--- ships/player.py
+++ ships/player.py
@@ -8,6 +8,7 @@ from ships.base import Base
 class Player(Base):
     def __init__(self, x, y):
         super().__init__(x, y)
+        self.velocity = 5
         # TODO: Figure this mess out. See if there is a better way
         dirname = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         self.img = pygame.image.load(os.path.join(dirname, 'assets', 'player_ship.png'))
