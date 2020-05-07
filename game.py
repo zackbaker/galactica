80b5a02fedcbab1524f8d1b0eef760ae7b5d83a1
Zack Baker <zbakerdesign@gmail.com>
2020-05-07 16:31:04 -0600

    Added images that potray movement, added ship collision, add colission with enemy lasers, added health and health bar to player. Added scoring and a game over menu

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ from ships.player import Player
 
 class Game:
     FRAME_RATE = 60
-    COLORS = {'white': (255, 255, 255)}
+    COLORS = {'white': (255, 255, 255), 'red': (255, 0, 0)}
 
     def __init__(self):
         pygame.init()
@@ -33,9 +33,10 @@ class Game:
         self.enemies = []
         self.lasers = {'player': [], 'enemy': []}
         self.level = 0
+        self.score = 0
         self.enemy_increment = 2
         # Full percentage, chance enemy will not shoot
-        self.enemy_shoot_chance = 96
+        self.enemy_shoot_chance = .04
 
     def main_menu(self):
         menu_font = self.small_font.render('Press any key to start', True, self.COLORS['white'])
@@ -64,6 +65,36 @@ class Game:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
                     self.game()
+                    break
+
+            pygame.display.update()
+
+    def game_over(self):
+        score_font = self.big_font.render('Score: {score}'.format(score=self.score), True, self.COLORS['white'])
+        score_font_y = (self.height / 2) - (score_font.get_height() / 2)
+        game_over_font = self.big_font.render('GAME OVER', True, self.COLORS['red'])
+        instructions_font = self.small_font.render('Press enter to replay', True, self.COLORS['white'])
+
+        while True:
+            self.window.blit(self.bg, (0, 0))
+            self.window.blit(game_over_font, ((self.width / 2) - (game_over_font.get_width() / 2), 10))
+            self.window.blit(
+                score_font,
+                ((self.width / 2) - (score_font.get_width() / 2), score_font_y)
+            )
+            self.window.blit(
+                instructions_font,
+                ((self.width / 2) - (instructions_font.get_width() / 2), score_font_y + score_font.get_height() + 10)
+            )
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                    self.score = 0
+                    self.level = 0
+                    self.game()
+                    break
 
             pygame.display.update()
 
@@ -83,7 +114,24 @@ class Game:
                 for enemy in self.enemies:
                     if self.check_collision(laser, enemy):
                         self.lasers['player'].remove(laser)
-                        self.enemies.remove(enemy   )
+                        self.enemies.remove(enemy)
+                        self.score += 1
+
+            for laser in self.lasers['enemy']:
+                if self.check_collision(self.player, laser):
+                    self.lasers['enemy'].remove(laser)
+                    self.player.hit()
+
+            for enemy in self.enemies:
+                if self.check_collision(self.player, enemy):
+                    self.enemies.remove(enemy)
+                    self.player.hit()
+                    self.score += 1
+
+            if self.player.health <= 0:
+                self.game_over()
+                break
+
             self.move_lasers()
             self.move_enemies()
             self.enemy_shoot()
@@ -129,21 +177,28 @@ class Game:
 
     def enemy_shoot(self):
         for enemy in self.enemies:
-            if (random.randint(0, 100) + random.randint(0, 100)) / 2 > self.enemy_shoot_chance and enemy.can_shoot():
-                # TODO get rid of the minus 10 by trimming enemy ships down
-                self.lasers['enemy'].append(Laser(enemy.x + (enemy.get_width() / 2 - 10), enemy.y, enemy.laser_img))
+            if (random.random() + random.random()) / 2 < self.enemy_shoot_chance and enemy.can_shoot():
+                self.lasers['enemy'].append(Laser(
+                    enemy.x + (enemy.get_width() / 2) - 10,
+                    enemy.y + enemy.get_height(),
+                    enemy.laser_img
+                ))
 
     def check_player_inputs(self, keys):
         if (keys[pygame.K_w] or keys[pygame.K_UP]) and 0 < (self.player.y + self.player.velocity):
             self.player.y -= self.player.velocity
+            self.player.img = self.player.move_img
         if (keys[pygame.K_s] or keys[pygame.K_DOWN]) \
                 and self.height > ((self.player.y + self.player.get_height()) + self.player.velocity):
             self.player.y += self.player.velocity
+            self.player.img = self.player.move_img
         if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and 0 < (self.player.x + self.player.velocity):
             self.player.x -= self.player.velocity
+            self.player.img = self.player.move_img
         if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) \
                 and self.width > ((self.player.x + self.player.get_width()) + self.player.velocity):
             self.player.x += self.player.velocity
+            self.player.img = self.player.move_img
         if keys[pygame.K_SPACE] and self.player.can_shoot():
             # TODO: Trim up images and get rid of the + 10
             # minus 10 is to get the laser to line up with the center of the ship
@@ -151,10 +206,17 @@ class Game:
                 Laser(self.player.x + ((self.player.get_width() / 2) - 10), self.player.y, self.player.laser_img)
             )
 
+        if not keys[pygame.K_w] and not keys[pygame.K_UP]:
+            self.player.img = self.player.still_img
+
     def draw(self):
         self.window.blit(self.bg, (0, 0))
+
         level_font = self.small_font.render('Level: {level}'.format(level=self.level), True, self.COLORS['white'])
         self.window.blit(level_font, (self.width - (level_font.get_width() + 10), 10))
+
+        score_font = self.small_font.render('Score: {score}'.format(score=self.score), True, self.COLORS['white'])
+        self.window.blit(score_font, (10, 10))
 
         for (laser_group, lasers) in self.lasers.items():
             for laser in lasers:
